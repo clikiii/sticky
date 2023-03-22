fe02d5fdf9b6cccf696206a3c6e8824838ae98ff
wangyian <naiy@wangyians-MacBook-Pro.local>
2023-03-22 20:08:46 +0800

    initial commit

---
+++ sticky/stickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  stickyApp.swift
+//  sticky
+//
+//  Created by wangyian on 4/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct stickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
